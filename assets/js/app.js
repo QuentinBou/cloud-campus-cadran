c26ad78af7717c870830ac8728479579fccf9a33
Quentin Bourgeois <quentintgc@live.fr>
2022-03-09 16:31:16 +0100

    second

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,18 @@
-const myPassword = 4016
-
 const mainContainer = document.querySelector('.main')
 const successMsg = document.querySelector('.success')
 
 const redLight = document.querySelector('.red')
 const greenLight = document.querySelector('.green')
 
-const firstHelp = document.querySelector('.first-help')
-const secondHelp = document.querySelector('.second-help')
-
 const buttons = document.querySelectorAll('.container div')
 
-const numbers = document.querySelectorAll('span')
+let numbers;
 
 let numberIter = 0;
 
 let errorNbr = 0;
+
+let myPassword;
 
 const playError = () => {
     const audio = new Audio("assets/audio/error.mp3")
@@ -37,6 +34,12 @@ const playButton = () => {
 
 const playApplause = () => {
     const audio = new Audio("assets/audio/applause.mp3")
+    audio.volume = 0.2
+    audio.play()
+}
+
+const playClick = () => {
+    const audio = new Audio("assets/audio/click.mp3")
     audio.volume = 0.2
     audio.play()
 }
@@ -68,36 +71,58 @@ const blinkBoth = () => {
 const successAnim = () => {
     mainContainer.style.opacity = 0
     mainContainer.style.left = "-50%"
-    firstHelp.style.opacity = 0
-    secondHelp.style.opacity = 0
-    firstHelp.style.transform = "scale(0)"
-    secondHelp.style.transform = "scale(0)"
     setTimeout(() => {
         mainContainer.style.display = "none";
         successMsg.style.visibility = "visible";
         successMsg.style.transform = "translate(-50%, -50%) scale(1)"
+        successAlert()
     }, 1750);
+}
+
+const successAlert = () => {
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        icon: 'success',
+        timerProgressBar: true,
+        title: "Bien joué !"
+    })
+}
+
+const sleep = timeMs => {
+    return new Promise(resolve => setTimeout(resolve, timeMs))
+}
+
+const hackingPassword = async () => {
+    document.querySelector('.skip').style.visibility = "hidden"
+    document.querySelector('h2').style.visibility = "hidden"
+    let passwordArray = myPassword.toString().split("")
+    for (let i = 0; i <= passwordArray.length - 1; i++){
+        let nextNum = passwordArray[i]
+        numbers[i].textContent = nextNum
+        for (const button of buttons) {
+            if (button.textContent == nextNum){
+                button.style.backgroundColor = "green"
+            }
+        }
+        playClick()
+        await sleep(1000)
+    }
+    blinkBoth()
+    
 }
 
 const clickNumber = (e) => {
     let number = parseInt(e.target.textContent);
     if (numberIter < myPassword.toString().split('').length){
         playButton()
-        checkNumber(number)
+        checkNumber(number, e)
     }
 }
 
-const checkError = () => {
-    if (errorNbr == 2){
-        firstHelp.style.visibility = "visible"
-    }
-    if (errorNbr == 4){
-        secondHelp.style.visibility = "visible"
-    }
-    errorNbr += 1
-}
-
-const checkNumber = (num) => {
+const checkNumber = (num, event) => {
     let passwordArray = myPassword.toString().split("");
     if (numbers[numberIter].textContent === "*" && num === parseInt(passwordArray[numberIter])){
         if (numberIter == numbers.length - 1){
@@ -105,18 +130,39 @@ const checkNumber = (num) => {
         } else {
             blinkCircle(greenLight, 'green-blink')
         }
+        event.target.style.backgroundColor = "green"
         setTimeout(() => {
             numbers[numberIter].textContent = num
             numberIter += 1
         }, 2000);
-    } else {
+    } else if (myPassword.toString().split("").includes(event.target.textContent)) {
+        event.target.style.backgroundColor = "pink"
+    }  else {
         blinkCircle(redLight, 'red-blink')
         playError()
-        checkError()
     }
     
+}
+
+const generatePassword = (length) => {
+    let passwordArray = [];
+    for (let i = 0; i < length; i++){
+        passwordArray[i] = Math.floor(Math.random() * 10)
+    }
+    return passwordArray.join('')
 }
 
 for (const button of buttons) {
     button.addEventListener('click', clickNumber)
 }
+document.querySelector('.skip').addEventListener('click', hackingPassword)
+
+window.addEventListener('load', () => {
+    myPassword = generatePassword(4)
+    for (let i = 0; i < myPassword.length; i++){
+        let newNumber = document.createElement('span')
+        newNumber.textContent = "*"
+        document.querySelector('.password').appendChild(newNumber)
+    }
+    numbers = document.querySelectorAll('span')
+})
